8ce2e99e6306ae62ff36a5def7a4a32d0d473c69
Seda Sebep <154930382+sedasebep@users.noreply.github.com>
2024-03-04 19:01:58 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-const frontendDirectory = "aman_frontend";
+const frontendDirectory = "ama_frontend";
 
 const frontend_entry = path.join("src", frontendDirectory, "src", "index.html");
 
